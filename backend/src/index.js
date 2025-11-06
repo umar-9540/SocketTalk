2ecd6fc9eed0c3513f4667fca89fffddc933907b
Umar Siddiqui <u9540776786@gmail.com>
2025-11-07 00:47:53 +0530

    Added Signup Page

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,6 +14,12 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
